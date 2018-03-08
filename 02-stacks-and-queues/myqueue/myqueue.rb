7d461389ce3848c9ab275b2c1e66da9e9483cc34
Arthur Philadelpho <arthur22rj@gmail.com>
2018-03-07 22:36:23 -0300

    Created MyQueue class.

--- 02-stacks-and-queues/myqueue/myqueue.rb
+++ 02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,30 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = nil
   end
 
   def enqueue(element)
+    @queue[@queue.length] = element
+    @head = @queue[0]
+    @tail = element
+
   end
 
   def dequeue
+    if !@queue.empty?
+      element = @queue.slice!(0)
+      @head = @queue[1]
+      return element
+    end  
   end
 
   def empty?
+    if @queue.length <=0
+      true
+    else
+      false
+    end
   end
+
 end
