1ba81a0a9b5ecabf7ed7122c194c7a09bc782709
Arthur Philadelpho <arthur22rj@gmail.com>
2018-03-07 21:13:41 -0300

    Created MyStack class.

--- 02-stacks-and-queues/mystack/mystack.rb
+++ 02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,23 @@ class MyStack
   end
 
   def push(item)
+      @stack[@stack.length] = item
+      self.top = item
   end
 
   def pop
+    if !@stack.empty?
+      item = @stack.slice!(@stack.length-1)
+      self.top = @stack[@stack.length-2]
+      return item
+    end
   end
 
   def empty?
+    if @stack.length <= 0
+      true
+    else
+      false 
+    end
   end
 end
